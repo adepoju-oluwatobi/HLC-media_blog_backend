8ec42a737b794e1cf5b2b74199ff35a13119455e
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-27 19:08:52 +0100

    edited

--- index.js
+++ index.js
@@ -7,49 +7,31 @@ const port = 3000;
 app.use(cors());
 
 //routes
-app.get("/api/data", (req, res) => {
-  try {
-    const data = { message: "Hello from the server!" };
-    res.json(data)
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.get("/api/user", (req, res) => {
+app.get("/api/post", (req, res) => {
   try{
-      const user = [{
+      const post = [{
         id: 1,
         title: "Cat drown of alcohol",
         des: "loreu ipium kkkkk.."
       },
-      {
-      id: 2,
-      name: "Rachael",
-      email: "missrachael41@gmail.com"
-      }
     ];
-    res.json(user);
+    res.json(post);
   } catch (error) {
     res.status(500).json({error: "Cannot fetch user"});
   }
 })
 
+//announcement API
 app.get("/api/announcement", (req,res) => {
  try {
-   const post = [
+   const announcement = [
      {
        id: 1,
        title: "School resumes on Monday",
        des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...",
      },
-     {
-       id: 2,
-       title: "Rachael",
-       des: "missrachael41@gmail.com",
-     },
    ];
-   res.json(post);
+   res.json(announcement);
  } catch (error) {
    res.status(500).json({ error: "Cannot fetch user" });
  }
