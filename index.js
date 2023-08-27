d15a28ddc6155794100ae3d793b815f4ffc4cdf9
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-27 21:39:05 +0100

    fixed

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const fs = require("fs");
 const path = require("path");
 
 app.use(cors());
+app.use(express.json());
+
+
+// Define a route to serve static files (images)
+app.use('/images', express.static('public/images'));
 
 const databaseDirectory = path.join(__dirname, "database");
 
@@ -17,6 +22,22 @@ app.get("/api/post", (req, res) => {
     res.json(postData);
   } catch (error) {
     res.status(500).json({ error: "Cannot fetch posts" });
+  }
+});
+
+app.post("/api/post", (req, res) => {
+  try {
+    const postFilePath = path.join(databaseDirectory, "post.json");
+    const postData = JSON.parse(fs.readFileSync(postFilePath, "utf-8"));
+
+    const newPost = req.body; // Assuming the request body contains the new post data
+    postData.push(newPost);
+
+    fs.writeFileSync(postFilePath, JSON.stringify(postData, null, 2));
+
+    res.status(201).json({ message: "Post added successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Cannot add post" });
   }
 });
 
