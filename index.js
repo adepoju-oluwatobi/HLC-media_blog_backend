397e7f332d569c69ecd350e8a6cea854b2e61d49
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-27 20:02:33 +0100

    fixed

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3000;
+const fs = require("fs");
+const path = require("path");
 
 app.use(cors());
 
-
-const dataDirectory = path.join(__dirname, "data");
+const databaseDirectory = path.join(__dirname, "database");
 
 app.get("/api/post", (req, res) => {
   try {
-    const postFilePath = path.join(dataDirectory, "./database/post.json");
+    const postFilePath = path.join(databaseDirectory, "post.json");
     const postData = JSON.parse(fs.readFileSync(postFilePath, "utf-8"));
     res.json(postData);
   } catch (error) {
@@ -19,36 +20,16 @@ app.get("/api/post", (req, res) => {
   }
 });
 
-//routes
-// app.get("/api/post", (req, res) => {
-//   try{
-//       const post = [{
-//         id: 1,
-//         title: "Cat drown of alcohol",
-//         des: "loreu ipium kkkkk.."
-//       },
-//     ];
-//     res.json(post);
-//   } catch (error) {
-//     res.status(500).json({error: "Cannot fetch user"});
-//   }
-// })
-
 //announcement API
-app.get("/api/announcement", (req,res) => {
- try {
-   const announcement = [
-     {
-       id: 1,
-       title: "School resumes on Monday",
-       des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...",
-     },
-   ];
-   res.json(announcement);
- } catch (error) {
-   res.status(500).json({ error: "Cannot fetch user" });
- }
-})
+app.get("/api/announcement", (req, res) => {
+  try {
+    const postFilePath = path.join(databaseDirectory, "announcement.json");
+    const postData = JSON.parse(fs.readFileSync(postFilePath, "utf-8"));
+    res.json(postData);
+  } catch (error) {
+    res.status(500).json({ error: "Cannot fetch posts" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
